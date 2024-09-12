973d3486efa7349d18ff421ae7c2cb5152d7ce3b
loick <loickwalle@gmail.com>
2024-09-12 16:56:53 +0200

    Ajout du projet IDEA

---
+++ FrigoMiamBack/src/main/java/com/example/FrigoMiamBack/FrigoMiamBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.FrigoMiamBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrigoMiamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrigoMiamBackApplication.class, args);
+	}
+
+}
