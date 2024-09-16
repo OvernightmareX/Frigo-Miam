78d741082361af9e9d6821487048d68d9ddfee92
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-16 14:13:51 +0200

    form create account

--- FrigoMiamFront/src/app/pages/connexion/connexion.component.ts
+++ FrigoMiamFront/src/app/pages/connexion/connexion.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
 
+
+
+
+subscribeAccount(){
+
+}
 }
