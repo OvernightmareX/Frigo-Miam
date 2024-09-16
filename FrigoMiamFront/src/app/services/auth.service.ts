c03c783e4930860e1b62d456129fb603aaaef559
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-16 16:57:34 +0200

    create authService authentified user

---
+++ FrigoMiamFront/src/app/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, of, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+
+  register(user: Partial<User>): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/account`, user).pipe(
+      catchError(error => {
+        alert(error.message);
+        return of();
+      })
+    );
+  }
+
+  login(cred: Pick<User, 'email' | 'password'>): Observable<{ accessToken: string }> {
+    return this.http.post<{ accessToken: string }>(`${this.apiUrl}/login`, cred).pipe(
+      tap(res => localStorage.setItem('token', res.accessToken)),
+      catchError(error => {
+        alert(error.message);
+        return of();
+      })
+    );
+  }
+
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
+
+
+
