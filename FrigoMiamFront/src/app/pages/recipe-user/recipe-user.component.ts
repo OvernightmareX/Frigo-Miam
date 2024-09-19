5a57ab2195de8103f0731055907940bf8b074650
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-19 09:53:49 +0200

    dependance fontawesomes

---
+++ FrigoMiamFront/src/app/pages/recipe-user/recipe-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-user.component.html',
+  styleUrl: './recipe-user.component.css'
+})
+export class RecipeUserComponent {
+
+}
