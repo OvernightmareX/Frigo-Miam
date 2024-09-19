ced1854690a240329b848405c3cec8644fb4a6a0
loick <loickwalle@gmail.com>
2024-09-19 10:12:08 +0200

    Correction DBInit

--- FrigoMiamBack/src/main/java/com/example/FrigoMiamBack/services/DBInitService.java
+++ FrigoMiamBack/src/main/java/com/example/FrigoMiamBack/services/DBInitService.java
@@ -1,5 +1,6 @@
 package com.example.FrigoMiamBack.services;
 
+import com.example.FrigoMiamBack.DTO.IngredientQuantityDTO;
 import com.example.FrigoMiamBack.entities.*;
 import com.example.FrigoMiamBack.repositories.IngredientRepository;
 import com.example.FrigoMiamBack.repositories.RecipeIngredientRepository;
@@ -67,81 +68,74 @@ public class DBInitService {
 
         List<Recipe> recipes = loadJSONRecipe();
 
+        List<IngredientQuantityDTO> allIngredients = new ArrayList<>();
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(0),2));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(13),50));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(12),10));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(50),5));
+        recipeService.addRecipe(recipes.get(0), adminCreated, allIngredients);
 
-        for(Recipe recipe : recipes){
-            recipe.setAccount(admin);
-            recipeRepository.save(recipe); // Save the recipe to the database
-            adminCreated.getRecipeCreatedList().add(recipe);
-        }
-
-
-        List<Recipe_Ingredient> allIngredients = new ArrayList<>();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(0), ingredients.get(0),2));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(0), ingredients.get(13),50));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(0), ingredients.get(12),10));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(0), ingredients.get(50),5));
-        this.recipeIngredientRepository.saveAll(allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(1), ingredients.get(7),200));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(1), ingredients.get(10),100));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(1), ingredients.get(11),5));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(1), ingredients.get(5),10));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(7),200));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(10),100));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(11),5));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(5),10));
+        recipeService.addRecipe(recipes.get(1), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(2), ingredients.get(1),300));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(2), ingredients.get(44),1));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(2), ingredients.get(13),20));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(1),300));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(44),1));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(13),20));
+        recipeService.addRecipe(recipes.get(2), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(3), ingredients.get(6),200));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(3), ingredients.get(11),10));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(3), ingredients.get(19),5));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(3), ingredients.get(13),20));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(6),200));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(11),10));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(19),5));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(13),20));
+        recipeService.addRecipe(recipes.get(3), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(4), ingredients.get(37),200));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(4), ingredients.get(11),5));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(4), ingredients.get(12),10));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(4), ingredients.get(9),20));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(37),200));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(11),5));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(12),10));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(9),20));
+        recipeService.addRecipe(recipes.get(4), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(5), ingredients.get(16),2));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(5), ingredients.get(22),1));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(5), ingredients.get(4),2));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(16),2));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(22),1));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(4),2));
+        recipeService.addRecipe(recipes.get(5), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(6), ingredients.get(25),300));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(6), ingredients.get(28),150));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(6), ingredients.get(17),30));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(6), ingredients.get(12),10));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(25),300));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(28),150));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(17),30));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(12),10));
+        recipeService.addRecipe(recipes.get(6), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(7), ingredients.get(10),150));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(7), ingredients.get(59),100));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(7), ingredients.get(12),10));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(7), ingredients.get(5),5));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(10),150));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(59),100));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(12),10));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(5),5));
+        recipeService.addRecipe(recipes.get(7), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(8), ingredients.get(35),300));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(8), ingredients.get(11),5));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(8), ingredients.get(14),20));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(8), ingredients.get(47),5));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(35),300));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(11),5));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(14),20));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(47),5));
+        recipeService.addRecipe(recipes.get(8), adminCreated, allIngredients);
 
         allIngredients.clear();
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(9), ingredients.get(0),200));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(9), ingredients.get(8),100));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(9), ingredients.get(2),50));
-        allIngredients.add(new Recipe_Ingredient(null, recipes.get(9), ingredients.get(17),30));
-        this.recipeIngredientRepository.saveAll(allIngredients);
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(0),200));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(8),100));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(2),50));
+        allIngredients.add(new IngredientQuantityDTO( ingredients.get(17),30));
+        recipeService.addRecipe(recipes.get(9), adminCreated, allIngredients);
 
         accountService.updateAccount(adminCreated);
     }
