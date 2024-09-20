e611f5f15f256c83a81fef6629728903df87573c
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-20 11:25:52 +0200

    recipe create form front

--- FrigoMiamFront/src/app/pages/recipe-create/recipe-create.component.ts
+++ FrigoMiamFront/src/app/pages/recipe-create/recipe-create.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
-
 @Component({
   selector: 'app-recipe-create',
   standalone: true,
@@ -29,46 +28,53 @@ export class RecipeCreateComponent {
     ])
     });
 
-  recipe = {
-    title: '',
-    descript: '',
-    instructions:'',
-    preparation_time:'',
-    cooking_time:'',
-    diet:[],
-    ingredients: [] as { ingredient: string, quantity: number, value: number }[],
-
-
-  };
+  // recipe = {
+  //   title: '',
+  //   descript: '',
+  //   instructions:'',
+  //   preparation_time:'',
+  //   cooking_time:'',
+  //   diet:[],
+  //   ingredients: [] as { ingredient: string, quantity: number, value: number }[],
+  //
+  //
+  // };
 
   isSubmitted = false;
 
-  newIngredient: { ingredient: string; quantity: number, value: number } = { ingredient: '', quantity: 1, value: 2 };
+  //newIngredient: { ingredient: string; quantity: number, value: number } = { ingredient: '', quantity: 1, value: 2 };
 
   constructor() { }
 
 
   addIngredient() {
-    console.log("In addIngredient")
-    if (this.newIngredient.ingredient && this.newIngredient.quantity > 0) {
-      console.log("PROCESS addIngredient - after if")
 
-      //this.recipe.ingredients.push({ ...this.newIngredient });
-      //this.newIngredient = { ingredient: '', quantity: 1, value: 2 };
-      this.createRecipe.controls.addQt.push(
-        new FormGroup({
-          ingredient: new FormControl(''),
-          quantity: new FormControl('')
-        })
-      )
+    const addQtArray = this.createRecipe.get('addQt') as FormArray;
+    addQtArray.push(new FormGroup({
+      ingredient: new FormControl(''),
+      quantity: new FormControl('')
+    }));
+
+
+    // console.log("In addIngredient")
+    // //if (this.createRecipe && this.newIngredient.quantity > 0) {
+    // //console.log("PROCESS addIngredient - after if")
+    //
+    // //this.recipe.ingredients.push({ ...this.newIngredient });
+    // //this.newIngredient = { ingredient: '', quantity: 1, value: 2 };
+    // this.createRecipe.controls.addQt.push(
+    //   new FormGroup({
+    //     ingredient: new FormControl(''),
+    //     quantity: new FormControl('')
+    //   })
+    // )
     }
-  }
+
 
   submitRecipe() {
+    console.log(`Recette soumise: ${JSON.stringify(this.createRecipe.value)}`, );
+
     this.isSubmitted = true;
-    console.log('Recette soumise', this.recipe);
   }
-
-
 }
 
