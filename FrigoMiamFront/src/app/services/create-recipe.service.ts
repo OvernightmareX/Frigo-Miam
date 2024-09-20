5b93636f5d0869409fcc8f325a83866b414f681a
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-20 16:40:21 +0200

    error merge

--- FrigoMiamFront/src/app/services/create-recipe.service.ts
+++ FrigoMiamFront/src/app/services/create-recipe.service.ts
@@ -7,7 +7,7 @@ import {Observable} from "rxjs";
 })
 export class CreateRecipeService {
 
-  private apiUrl: 'http://localhost:8080';
+  apiUrl: string = 'http://localhost:8080';
 
 
   constructor( private http: HttpClient) { }
